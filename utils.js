07cb5de57164f06264b0da913b0c3a004212a241
chenyang.zhou <chenyang.zhou@leyantech.com>
2021-12-03 10:14:30 +0800

    feat(add): 修改utils

--- utils.js
+++ utils.js
@@ -1,5 +1,9 @@
+const CryptoJS = require('crypto-js');
+
 var window = {
     atob :
         function (strText){return new Buffer.from(strText, "base64").toString("binary")},
     btoa: function (strText){new Buffer.from(strText, "binary").toString("base64")}
 }
+
+

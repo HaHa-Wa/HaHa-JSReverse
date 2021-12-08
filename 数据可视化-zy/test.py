f6bcfd0f52dad1eb16f85d79ea153c2ae54293e8
chenyang.zhou <chenyang.zhou@leyantech.com>
2021-12-08 19:35:12 +0800

    feat(add): 添加test

--- 数据可视化-zy/test.py
+++ 数据可视化-zy/test.py
@@ -1,22 +1,18 @@
-# x= 0
-# a = 8
-# b = 4
-# # y = round(((1 - a * a * x * x) / (b * b)) ** 0.5, 8)
-# y = (1 - a * a * x * x) / (b * b)
-# print(y)
+# https://blog.csdn.net/weixin_30819085/article/details/98844077
 import numpy as np
 import matplotlib.pyplot as plt
 
 z = -np.linspace(9,15,100)
 x = np.linspace(-26,26,1000)
-# print(z)
 x, z = np.meshgrid(x, z)
 Z = -np.exp(-0.05*z) + 4 * (z+10)**2
 X = x**2
 print(type(X+Z))
 
-
+# 绘制轮廓线
 plt.contour(x, z, (X+Z), [0])
+
+# 限制横轴 纵轴大小
 plt.xlim([-1.5,1.5])
 plt.ylim([-11.5,-8.5])
 plt.show()

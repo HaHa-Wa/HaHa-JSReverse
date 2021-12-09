b0eff16dab47f8a60e85151df2954440b0f652c6
chenyang.zhou <chenyang.zhou@leyantech.com>
2021-12-09 17:33:26 +0800

    feat(add): 添加headers hook

--- utils.js
+++ utils.js
@@ -1,23 +1,51 @@
 const CryptoJS = require('crypto-js');
 
 var window = {
-    atob :
-        function (strText){return new Buffer.from(strText, "base64").toString("binary")},
-    btoa: function (strText){new Buffer.from(strText, "binary").toString("base64")}
+    atob:
+        function (strText) {
+            return new Buffer.from(strText, "base64").toString("binary")
+        },
+    btoa: function (strText) {
+        new Buffer.from(strText, "binary").toString("base64")
+    }
 }
 
 // 定时器
-for (var i = 1; i < 99999; i++){
+for (var i = 1; i < 99999; i++) {
     window.clearInterval(i);
 }
 
 // headers hook
-(function() {
+(function () {
     var org = window.XMLHttpRequest.prototype.setRequestHeader;
-    window.XMLHttpRequest.prototype.setRequestHeader = function(key, value) {
-        if (key == 'lxlxlx') {
+    window.XMLHttpRequest.prototype.setRequestHeader = function (key, value) {
+        if (key == 'kl_sign') {
             debugger;
         }
         return org.apply(this, arguments);
     }
+})()
+
+
+    // url hook
+    (function () {
+        var open = window.XMLHttpRequest.prototype.open;
+        window.XMLHttpRequest.prototype.open = function (method, url, async) {
+            if (url.indexOf("p") > -1) {
+                debugger;
+            }
+            return open.apply(this, arguments);
+        };
+    })()
+
+// cookie hook
+(function(){
+    var org = document.cookie.__lookupSetter__('cookie');
+    document.__defineSetter__("cookie",function(cookie){
+        if(cookie.indexOf('m')>-1){
+            debugger;
+        }
+        org = cookie;
+    });
+    document.__defineGetter__("cookie",function(){return org;});
 })()

89b6e18288547e459374d60b5f3996ed20c9e303
chenyang.zhou <chenyang.zhou@leyantech.com>
2021-12-10 19:06:51 +0800

    feat(add): 添加headers hook

---
+++ JS逆向-H5-玩物得志/JS逆向-H5-玩物得志.js
@@ -0,0 +1,3 @@
+!function (t, e) {
+    "object" == typeof exports && "object" == typeof module ? module.exports = e(require("axios")) : "function" == typeof define && define.amd ? define("WWDZFN", ["axios"], e) : "object" == typeof exports ? exports.WWDZFN = e(require("axios")) : t.WWDZFN = e(t.axios)
+}

64080cd716e7679f1aa7ac683847d887f607b58e
chenyang.zhou <chenyang.zhou@leyantech.com>
2022-02-14 18:11:34 +0800

    feat(update): 健康预约房间

--- 健康预约/main.py
+++ 健康预约/main.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 import base64
 import hashlib
@@ -26,6 +28,19 @@ def getVerify():
     print(res)
     return res
 
+def whilePost():
+    while True:
+        data = {
+            "certType": 4,
+            "verifyCode": getVerify(),
+            "certNo": certNo,
+            "pwd": pwd
+        }
+        ret = session_info.post(url, data=data).json()
+        print(ret)
+        if ret.get('status') != 500:
+            break
+        time.sleep(0.05)
 
 def login():
     account = 'H60289244'
@@ -33,29 +48,31 @@ def login():
     certNo = base64encode(account)
     pwd = base64encode((md5(password)))
     url = 'https://hk.sz.gov.cn:8118/user/login'
-    data = {
-        "certType": 4,
-        "verifyCode": getVerify(),
-        "certNo": certNo,
-        "pwd": pwd
+
+
+
+
+def gethotalInfo():
+    hotalInfoUrl = 'https://hk.sz.gov.cn:8118/districtHousenumLog/getList'
+    hotalInfoData = {
     }
-
-    ret = session_info.post(url, data=data).json()
-    print(ret)
-    if ret.get('status') == 500:
-        return
-    haha = session_info.post('https://hk.sz.gov.cn:8118/passInfo/getCheckInInfoList', data={"pageIndex": 1,
-                                                                                            "pageSize": 10})
-    print(haha.text)
-
-    # return certNo, pwd
+    res = session_info.post(url=hotalInfoUrl, data=hotalInfoData).json()
+    hotels = res['data']
+    print(hotels)
+    for hotel in hotels:
+        print(hotel)
+        if hotel['data'] == checkTime:
+            print(hotel)
 
 
 if __name__ == '__main__':
+    checkTime = '2022-02-15'
+
     session_info = requests.Session()
     headers = {
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
         'Cookie': 'isolateLocale=zh-CN; isolate-web-session-id=62266044-86ce-4ab5-90a0-2e1139b14c95'
     }
     login()
-    # getVerify()
+
+    gethotalInfo()

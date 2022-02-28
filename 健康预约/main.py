f0ad7f224536e6dd67f5eb9c852c6154d14fe3d1
chenyang.zhou <chenyang.zhou@leyantech.com>
2022-02-28 10:48:06 +0800

    feat(update): 更新健康登记

--- 健康预约/main.py
+++ 健康预约/main.py
@@ -1,15 +1,18 @@
+import os
 import time
-
 import requests
 import base64
 import hashlib
 import ddddocr
+import threading
+from concurrent.futures import ThreadPoolExecutor
+
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 def md5(str):
     m = hashlib.md5()
     m.update(str.encode("utf8"))
-    print(m.hexdigest())
     return m.hexdigest()
 
 
@@ -19,10 +22,12 @@ def base64encode(str1):
 
 
 def getVerify():
+    print('正在获取验证码')
     verifyUrl = 'https://hk.sz.gov.cn:8118/user/getVerify'
-    ret = session_info.get(verifyUrl)
+    ret = session_info.get(verifyUrl, headers=headers)
     with open('ver.jpeg', 'wb') as f:
         f.write(ret.content)
+    print('正在识别')
     ocr = ddddocr.DdddOcr()
     res = ocr.classification(ret.content)
     print(res)
@@ -36,6 +41,8 @@ def whilePost():
 def submitReservation(hotel):
     url = 'https://hk.sz.gov.cn:8118/passInfo/submitReservation'
     while True:
+        # pool_lock.acquire(blocking=True, timeout=1)
+        # pool_lock.locked()
         data = {
             'checkinDate': hotel['date'],
             'checkCode': getVerify(),
@@ -43,15 +50,15 @@ def submitReservation(hotel):
             't': hotel['timespan'],
             's': hotel['sign']
         }
-        ret = session_info.post(url, data=data).json()
+        ret = session_info.post(url, data=data, headers=headers).json()
         print(ret)
         if ret['status'] != 500:
             print('预约成功')
+            # os._exit(0)
             break
 
 
 def login():
-
     certNo = base64encode(account)
     pwd = base64encode((md5(password)))
     url = 'https://hk.sz.gov.cn:8118/user/login'
@@ -62,7 +69,7 @@ def login():
             "certNo": certNo,
             "pwd": pwd
         }
-        ret = session_info.post(url, data=data).json()
+        ret = session_info.post(url, data=data, headers=headers).json()
         print(ret)
         if ret.get('status') != 500:
             break
@@ -74,11 +81,9 @@ def gethotalInfo():
     hotalInfoUrl = 'https://hk.sz.gov.cn:8118/districtHousenumLog/getList'
     hotalInfoData = {
     }
-    res = session_info.post(url=hotalInfoUrl, data=hotalInfoData).json()
+    res = session_info.post(url=hotalInfoUrl, data=hotalInfoData, headers=headers).json()
     hotels = res['data']
-    print(hotels)
     for hotel in hotels:
-        print(hotel)
         if hotel['date'] == checkTime:
             print(hotel)
             return hotel
@@ -87,8 +92,25 @@ def gethotalInfo():
 if __name__ == '__main__':
     account = 'H60289244'
     password = 'zxd5201314'
-    checkTime = '2022-02-15'
+    checkTime = '2022-02-19'
+    # pool = ThreadPoolExecutor(max_workers=2)
+    # pool_lock = threading.Lock()
+    headers = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
+    }
     session_info = requests.Session()
     login()
     hotel = gethotalInfo()
-    submitReservation(hotel)
+
+    sched = BlockingScheduler()
+    sched.add_job(submitReservation, trigger='cron', second=0, minute=0, hour=10, args=[hotel])
+    sched.start()
+    # for x in range(10):
+    # checkCode = getVerify()
+    # future1 = pool.submit(submitReservation, hotel)
+    # future2 = pool.submit(submitReservation, hotel)
+    # future3 = pool.submit(submitReservation, hotel)
+    # future4 = pool.submit(submitReservation, hotel)
+    # future5 = pool.submit(submitReservation, hotel)
+
+    # submitReservation(hotel)
